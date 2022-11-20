a5c8e518f603d5b4cf4561c4851e08b52938d602
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-11-19 21:27:05 -0500

    createAppointment && updateAppointment Test Done

--- repairnow-api/RepairNow.Domain.Test/AppointmentDomainUnitTest.cs
+++ repairnow-api/RepairNow.Domain.Test/AppointmentDomainUnitTest.cs
@@ -1,6 +1,63 @@
-﻿namespace RepairNow.Domain.Test;
+﻿using Moq;
+using RepairNow.Infraestructure;
+using Xunit;
+
+namespace RepairNow.Domain.Test;
 
 public class AppointmentDomainUnitTest
 {
+    [Fact]
+    public void Setup()
+    {
+    }
     
+    [Fact]
+    public void CreateAppointment_ReturnTrue()
+    {
+        var appointmentsRepository = new Mock<IAppointmentsRepository>();
+        appointmentsRepository.Setup(appointmentsRepository => appointmentsRepository.createAppointment(It.IsAny<Appointment>())).Returns(Task.FromResult(true));
+
+        var appointment = new Appointment()
+        {
+            dateReserve="19/10/22",
+            dateAttention="22/09/2022",
+            hour = "02:20",
+            clientId =1,
+            applianceModelId =1,
+        };
+        var appointmentDomain = new AppointmentsDomain(appointmentsRepository.Object);
+        var result = appointmentDomain.createAppointment(appointment);
+        Assert.True(result.Result);
+    }
+    
+    [Fact]
+    public void UpdateAppointment_ReturnTrue()
+    {
+        var appointmentsRepository = new Mock<IAppointmentsRepository>();
+        appointmentsRepository.Setup(appointmentsRepository => appointmentsRepository.updateAppointment(1,It.IsAny<Appointment>())).Returns(Task.FromResult(true));
+
+        
+        var oldAppointment = new Appointment()
+        {
+            id = 1,
+            dateReserve="19/10/22",
+            dateAttention="22/09/2022",
+            hour = "02:20",
+            applianceModelId =1,
+            clientId = 1,
+        };
+        var newAppointment = new Appointment()
+        {
+            dateReserve="10/10/22",
+            dateAttention="05/11/2022",
+            hour = "05:50",
+            applianceModelId =1,
+            clientId = 1,
+        };
+        var appointmentDomain = new AppointmentsDomain(appointmentsRepository.Object);
+        var createResult = appointmentDomain.createAppointment(oldAppointment);
+        var updateResult = appointmentDomain.updateAppointment(1,newAppointment);
+
+        Assert.True(updateResult.Result);
+    }
 }
---
+++ repairnow-api/RepairNow.Domain.Test/ReportDomainUnitTest.cs
@@ -0,0 +1,6 @@
+﻿namespace RepairNow.Domain.Test;
+
+public class ReportDomainUnitTest
+{
+    
+}
